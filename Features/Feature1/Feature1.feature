f9e5e6474d1782913db5ac4985edb0be17427852
Eduardo Flores <eduflornet@gmail.com>
2016-04-11 15:46:25 +0200

    Custom Control SharePoint Online
    Custom Control en Add-inn SharePoint Hosted para ver los pedidos que ha
    realizado un determinado cliente. Desde la lista de Contacto se enlaza
    este control el cual muestra el pedido de cada cliente enlaza con un
    Lookup

---
+++ Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="7b9f04db-26df-46b0-9a51-9ccf5d37edef" description="SharePoint Add-in Feature" featureId="7b9f04db-26df-46b0-9a51-9ccf5d37edef" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="CustomControlSharePointHosted Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="e541c8e8-679f-41d6-82a7-a5fcd09dbb17" />
+    <projectItemReference itemId="8072b569-192c-4f2c-ad18-444bcdc84369" />
+    <projectItemReference itemId="8ea6d33e-0227-4692-9ed9-45e05f17e986" />
+    <projectItemReference itemId="d8f256de-93b4-49f8-8d30-1d6e5faccf47" />
+  </projectItems>
+</feature>
---
+++ pkg/Debug/CustomControlSharePointHosted/CustomControlSharePointHosted_Feature1/Scripts/App.js
@@ -0,0 +1,99 @@
+﻿'use strict';
+
+function getQueryStringParams(sParam) {
+
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+var currentContext;
+var hostWebContext;
+var web;
+var list;
+
+function getList(id) {
+    clearInfo();
+    var lista = hostWebContext.get_web().get_lists().getByTitle("PedidosClientes");
+    // var idCliente = $("#idCliente").val();
+
+
+    var query = new SP.CamlQuery();
+    query.set_viewXml("<View Scope='RecursiveAll'><Query><Where><Contains><FieldRef Name='Cliente_x003a_ID' /><Value Type='Text'>" + id + "</Value></Contains></Where></Query></View>");
+    list = lista.getItems(query);
+
+    // TODO: Task 2. Submit query results
+    currentContext.load(list);
+    currentContext.executeQueryAsync(Function.createDelegate(this, onSuccess), Function.createDelegate(this, onFail));
+}
+
+function onSuccess() {
+
+    if (list.get_count() != 0) {
+
+        var listEnum = list.getEnumerator();
+        var n = 1;
+        var total = 0;
+        var tabla = $("#TablaPedidos tbody");
+        while (listEnum.moveNext()) {
+            var html = "<tr>";
+            
+            var actual = listEnum.get_current();
+            var d = actual.get_item("Fecha");
+            var day = d.getDate();
+            var month = d.getMonth() + 1;
+            var year = d.getFullYear();
+
+            html+="<td>"+ actual.get_item("Numero_x0020_pedido")+"</td>";
+            html+="<td>"+ day + "-"   +month+ "-" + year+"</td>";
+            html+="<td>"+ actual.get_item("Total") +"€ </td>";
+            n++;
+            tabla.append(html);
+        }
+        var nombre = actual.get_item("Cliente_x003a_Full_x0020_Name").get_lookupValue();
+        $("#Nombre").text(nombre);
+        $("#NumeroPedidos").text("Pedidos: " + (n - 1));
+
+        $("#MostrarPedidos").css("display", "block");
+        $("#SinPedidos").css("display", "none");
+
+    }
+    else {
+        $("#MostrarPedidos").css("display", "none");
+        $("#SinPedidos").css("display", "block");
+    }
+
+}
+
+function onFail() {
+    alert("Error");
+}
+
+function clearInfo() {
+    var t = $("#TablaPedidos>tbody>tr");
+    t.remove();
+    $("#NumeroPedidos").text("");
+    $("#Total").text("");
+}
+
+function init() {
+    var hostUrl = decodeURIComponent(getQueryStringParams("SPHostUrl"));
+    currentContext = new SP.ClientContext.get_current();
+    hostWebContext = new SP.AppContextSite(currentContext, hostUrl);
+    web = hostWebContext.get_web();
+
+    var id = getQueryStringParams("SPListItemId");
+    getList(id);
+
+}
+
+$(document).ready(function () {
+    ExecuteOrDelayUntilScriptLoaded(init, "sp.js");
+
+});
+
